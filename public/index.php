aa09755950e7a178b7abe884df93a737c9f2df41
Papp Krisztian <fejlesztes@letscode.hu>
2021-10-03 17:24:45 +0200

    Localization-4

--- public/index.php
+++ public/index.php
@@ -8,5 +8,15 @@ ini_set("display_errors", "1");
 require_once "../core/functions.php";
 require_once "../vendor/autoload.php";
 
+$locale= "hu_HU.utf8";
+
+if (defined("LC_MESSAGES")) {
+    setlocale(LC_MESSAGES, $locale);
+    bindtextdomain("messages", "../i18n");
+} else {
+    putenv("LC_ALL={$locale}");
+    bindtextdomain("messages", "..\i18n");
+}
+
 (new \Application(new ServiceContainer(include "../services.php")))->start(realpath(__DIR__. "/../"));
 
